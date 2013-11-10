639f17a60cac8e425d7cbffc7c148007a4c1811d
Iván Stepaniuk <istepaniuk@gmail.com>
2013-11-10 16:20:29 +0100

    #refactor, extract method + extract variable

--- StringDistance/LevenshteinDistanceCalculator.cs
+++ StringDistance/LevenshteinDistanceCalculator.cs
@@ -5,40 +5,62 @@ namespace Istepaniuk.StringDistance
 {
     public class LevenshteinDistanceCalculator
     {
-        public int Distance (string source, string target)
+        public int Distance(string source, string target)
         {
-            if (AnyStringIsNullOrEmpty (source, target))
-                return LengthOfTheLongestString (source, target);
+            if (AnyStringIsNullOrEmpty(source, target))
+                return LengthOfTheLongestString(source, target);
 
-            var n = source.Length;
-            var m = target.Length;
-            var score = new int[n + 1, m + 1];
+            var score = PrepareScoreMatrix(source, target);
 
-            for (var i = 0; i <= n; score[i, 0] = i++) {
-            }
-            for (var j = 0; j <= m; score[0, j] = j++) {
-            }
-            for (var i = 1; i <= n; i++) {
-                for (var j = 1; j <= m; j++) {
-                    var cost = (target [j - 1] == source [i - 1]) ? 0 : 1;
-                    score [i, j] = Math.Min (Math.Min (score [i - 1, j] + 1, score [i, j - 1] + 1), score [i - 1, j - 1] + cost);
+            for (var i = 1; i <= source.Length; i++)
+            {
+                for (var j = 1; j <= target.Length; j++)
+                {
+                    var cost = (target[j - 1] == source[i - 1]) ? 0 : 1;
+
+                    var delete = score[i - 1, j] + 1;
+                    var insert = score[i, j - 1] + 1;
+                    var replace = score[i - 1, j - 1] + cost;
+
+                    score[i, j] = Minimum(delete, insert, replace);
                 }
             }
 
-            return score [n, m];
+            return score[source.Length, target.Length];
         }
 
-        private bool AnyStringIsNullOrEmpty (string string1, string string2)
+        private int[,] PrepareScoreMatrix(string source, string target)
         {
-            return String.IsNullOrEmpty (string1)
-                || String.IsNullOrEmpty (string2);
+            var sourceLength = source.Length;
+            var targetLength = target.Length;
+            var score = new int[sourceLength + 1, targetLength + 1];
+
+            for (var i = 0; i <= sourceLength; score[i, 0] = i++)
+            {
+            }
+            for (var j = 0; j <= targetLength; score[0, j] = j++)
+            {
+            }
+
+            return score;
         }
 
-        private int LengthOfTheLongestString (params string[] strings)
+        private int Minimum(int int1, int int2, int int3)
+        {
+            return Math.Min(Math.Min(int1, int2), int3);
+        }
+
+        private bool AnyStringIsNullOrEmpty(string string1, string string2)
+        {
+            return String.IsNullOrEmpty(string1)
+                || String.IsNullOrEmpty(string2);
+        }
+
+        private int LengthOfTheLongestString(params string[] strings)
         {
             return strings
-                .Select (word => word ?? string.Empty)
-                    .Max (word => word.Length);
+                .Select(word => word ?? string.Empty)
+                .Max(word => word.Length);
         }
     }
 }
